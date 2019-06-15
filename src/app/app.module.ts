a46e363160cd0ac15880f17bb53a8b969a98ae4e
Georgy <louis.georgy@gmail.com>
2019-06-14 21:56:32 -0400

    basic service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 
+import { DishService } from './services/dish.service';
+
 import { MatListModule } from '@angular/material/list';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
@@ -32,7 +34,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatToolbarModule,
     FlexLayoutModule
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
