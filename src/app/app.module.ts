5b261f0b9b2fecf95a3162eb3e24104dcc277aa7
Georgy <louis.georgy@gmail.com>
2019-06-14 23:24:59 -0400

    Router Basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,11 @@ import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -26,7 +31,10 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -36,6 +44,7 @@ import { FooterComponent } from './footer/footer.component';
     MatCardModule,
     MatButtonModule,
     MatToolbarModule,
+    AppRoutingModule,
     FlexLayoutModule
   ],
   providers: [DishService],
